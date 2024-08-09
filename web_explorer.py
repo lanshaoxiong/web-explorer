004d6df4d62d53b084f2c645dd0765d2be21395a
Shaoxiong Lan <shaoxi@amazon.com>
2024-08-09 16:57:43 -0400

    first commit test

--- web_explorer.py
+++ web_explorer.py
@@ -5,6 +5,8 @@ from langchain.retrievers.web_research import WebResearchRetriever
 
 import os
 
+# First commit
+
 os.environ["GOOGLE_API_KEY"] = "YOUR_API_KEY" # Get it at https://console.cloud.google.com/apis/api/customsearch.googleapis.com/credentials
 os.environ["GOOGLE_CSE_ID"] = "YOUR_CSE_ID" # Get it at https://programmablesearchengine.google.com/
 os.environ["OPENAI_API_BASE"] = "https://api.openai.com/v1"
